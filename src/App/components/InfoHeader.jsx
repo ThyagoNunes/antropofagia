0bfda71cb491dec828f4bacc544be6fd9b1240f1
Thyago Nunes <devthyagonunes@gmail.com>
2023-08-14 12:10:16 -0300

    mobile infoHeader top adjusting

--- src/App/components/InfoHeader.jsx
+++ src/App/components/InfoHeader.jsx
@@ -1,7 +1,7 @@
 export function InfoHeader() {
   return (
     <>
-      <div className="mb:mx-[1px] mb:relative mb:w-screen mb:tracking-[.14em] mb:uppercase mb:text-white mb:flex mb:gap-11 mb:items-center mb:justify-around mb:top-[-17vh] mb:font-light mb:text-base 
+      <div className="mb:mx-[1px] mb:relative mb:w-screen mb:tracking-[.14em] mb:uppercase mb:text-white mb:flex mb:gap-11 mb:items-center mb:justify-around mb:top-[-15vh] mb:font-light mb:text-base 
       xxl:mx-2 xxl:relative xxl:h-24 xxl:w-screen xxl:text-6xl xxl:tracking-[.3em] xxl:uppercase xxl:text-white xxl:flex xxl:items-center xxl:justify-around xxl:gap-[190px] xxl:-top-[60.8vh] ">
         <span className="">gabriel marques</span>
         <span className="font-bold">antropofagia</span>
